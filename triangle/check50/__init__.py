10c205ab495777902f44b29d42e656ed8b4d3f87
shoutiss <shoutiss@newton.k12.ma.us>
2018-10-26 10:24:39 -0400

    Update __init__.py

--- triangle/check50/__init__.py
+++ triangle/check50/__init__.py
@@ -27,3 +27,9 @@ class Triangle(Checks):
         """input of -3, 4, 5 yields output of NO\n"""
         self.spawn("./triangle").stdin("-3").stdin("4").stdin("5").stdout("NO\n", "NO\n").exit(0)
 
+    @check("compiles")
+    def test_neg3_4_5(self):
+        """input of 2, 4, 6 yields output of NO\n"""
+        self.spawn("./triangle").stdin("2").stdin("4").stdin("6").stdout("NO\n", "NO\n").exit(0)
+
+
