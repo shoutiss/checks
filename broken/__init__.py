dffbc5227df815989f184ca9e6c1af2323209718
shoutiss <shoutiss@newton.k12.ma.us>
2019-10-17 09:11:48 -0400

    Update __init__.py

--- broken/__init__.py
+++ broken/__init__.py
@@ -10,4 +10,4 @@ def compiles():
 @check50.check(compiles)
 def sums_1_5():
     """finds the sum of 1 through 5 inclusive" """
-    check50.run("./broken").stdin(1).stdin(5).stdout("Sum: 15\n").exit(0)
+    check50.run("./broken").stdin("1").stdin("5").stdout("Sum: 15\n").exit(0)
