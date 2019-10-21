fd891a3942ae3324c237965e6c2177fe98f010c4
Sam Shoutis <shoutiss@newton.k12.ma.us>
2019-10-21 14:40:55 +0000

    Final version of test

--- twinprime/__init__.py
+++ twinprime/__init__.py
@@ -15,4 +15,4 @@ def compiles():
 @check50.check(compiles)
 def find_primes():
     """Finds the twin primes below 100."""
-    check50.run("./twinprime").stdout("3, 5\n").exit(0)
+    check50.run("./twinprime").stdout("3, 5\n5, 7\n11, 13\n17, 19\n29, 31\n41, 43\n59, 61\n71, 73").exit(0)
