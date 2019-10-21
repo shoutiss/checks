5f9d64c0db62f7d79cb6b0c4d82b86fbb83c9be6
Sam Shoutis <shoutiss@newton.k12.ma.us>
2019-10-21 14:38:49 +0000

    fixed init, I hope

--- twinprime/__init__.py
+++ twinprime/__init__.py
@@ -2,17 +2,17 @@
 import check50
 import check50.c
 
-# @check50.check()
-# def exists():
-#     """twinprime.c exists"""
-#     check50.exists("twinprime.c")
-
 @check50.check()
+def exists():
+    """twinprime.c exists"""
+    check50.exists("twinprime.c")
+
+@check50.check(exists)
 def compiles():
     """twinprime.c compiles"""
     check50.c.compile("twinprime.c", lcs50=True)
 
 @check50.check(compiles)
-def find_primes:
+def find_primes():
     """Finds the twin primes below 100."""
     check50.run("./twinprime").stdout("3, 5\n").exit(0)
